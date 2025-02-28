e94059da6c81f9d504252d82f1829f84c2be26a8
Badwolf_TW <howard93921@gmail.com>
2025-02-28 16:51:57 +0800

    fix

--- assistant/assistant.py
+++ assistant/assistant.py
@@ -114,8 +114,6 @@ class OpenAIChat(commands.Cog, AssistantCommands):
         )
         formatted_user_input = f"Discord User {user_name} (ID: <@{user_id}>) said:\n{user_input}"
 
-        await asyncio.sleep(default_delay)
-
         return self._blocking_openai_request(api_key, api_url_base, model, sysprompt, guild_history, formatted_user_input)
 
     def _blocking_openai_request(self, api_key: str, api_url_base: str, model: str, prompt: str, guild_history: str, user_input: str) -> Optional[str]:
@@ -158,12 +156,15 @@ class OpenAIChat(commands.Cog, AssistantCommands):
         ctx = await self.bot.get_context(message)
         if ctx.valid:
             return
-    
+        
         if str(message.channel.id) not in channels:
             return
 
         response = await self.query_openai(message)
-        await self.process_response(message, response)
+        if response:
+            await self.process_response(message, response)
+        
+        await asyncio.sleep(4)
 
     async def load_chat_history(self, guild_id: int):
         """Load chat history for a guild."""
